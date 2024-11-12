8532bac7100e0f50487496a968af6e768475bd4b
manishks2901 <manishks2901@icloud.com>
2024-11-12 23:29:13 +0530

    +

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,15 +1,20 @@
-import Image from "next/image";
 import RootNavbar from "@repo/ui/root-navbar-structure1";
 import MobileNavbar from "@repo/ui/mobile-navbar";
+import IntroPage from "../../components/intro-page";
 
 export default function Page(): JSX.Element {
   return (
     <>
-      <RootNavbar 
-        title="Manish Kumar Sharma ..."
-        MainNavbarLinks={() => <div>Links</div>}
-        MobileNavbar={MobileNavbar}
-      />
+      <div className="">
+        <RootNavbar 
+          title="Manish Kumar Sharma ..."
+          MainNavbarLinks={() => <div>Links</div>}
+          MobileNavbar={MobileNavbar}
+        />
+        <div className="ml-8 mr-8 mt-12 py-4">
+            <IntroPage/>
+        </div>
+      </div>
     </>
   );
 }
